55761cf30a3a39aae41b644e0150414f4760ecb8
Lyudmila17 <merkel.naller@gmail.com>
2019-01-17 17:40:59 +0100

    drivers: prima: Switch to wlan-driver.lnx.1.0.r30-rel
     * It's compatible and the most updated one
     * https://source.codeaurora.org/external/gigabyte/platform/vendor/qcom-opensource/wlan/prima/?h=caf/wlan-driver.lnx.1.0.r30-rel
    
    Change-Id: Ia52d833d90b9d273aa031362b0866c67c702ce39
    
    staging: prima: Add QCOM_TDLS config
    
    Read WLAN MAC addresses from UTAG
    
    Merged Original CR's to have single commit:
    (CR) Read WiFi MACs from bootargs
    (CR) Handle inter MAC separator in utags
    
    Factory sets 2 WiFi MACs as boot params. This code change
    reads WiFi MACs from bootparams device tree node and
    generates two other MACs by setting locally administrated
    bit. Order of MAC addresses is MAC1, MAC2, GENERATED1, and
    GENERATED2. From here on we don't read MAC addresses from
    NV bin file.
    Also handle cases none, comma and hypen as separator
    
    Change-Id: Ied2bddaaef14b0849e3f8adc57ef9e14754166f5
    Reviewed-on: http://gerrit.mot.com/833472
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ishwara J A <ishwara@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>
    
    Driver blocking scans while on BT call
    
    Allos wifi scans during BT sco call as is the case in titan/Condor/victara
    SR 01884803.
    
    Change-Id: Ie1f3e8202929d16a4b842a07f79231177446d1a6
    Reviewed-on: http://gerrit.mot.com/706275
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/838719
    SME-Granted: SME Approvals Granted
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limSmeReqUtils.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limSmeReqUtils.c
@@ -261,6 +261,7 @@ limSetRSNieWPAiefromSmeStartBSSReqMessage(tpAniSirGlobal pMac,
 {
     tANI_U8  wpaIndex = 0;
     tANI_U32 privacy, val;
+    tANI_U32 status;
 
     if (wlan_cfgGetInt(pMac, WNI_CFG_PRIVACY_ENABLED,
                   &privacy) != eSIR_SUCCESS)
@@ -335,7 +336,16 @@ limSetRSNieWPAiefromSmeStartBSSReqMessage(tpAniSirGlobal pMac,
                    LOG1,
                    FL("Only RSN IE is present"));
             dot11fUnpackIeRSN(pMac,&pRSNie->rsnIEdata[2],
+                              pRSNie->rsnIEdata[1],
                               (tANI_U8)pRSNie->length,&pSessionEntry->gStartBssRSNIe);
+            if (DOT11F_FAILED(status))
+            {
+                limLog(pMac,
+                       LOGE,FL("unpack failed for RSN IE (0x%08x)"),
+                       status);
+                return false;
+            }
+	    return true;
         }
         else if ((pRSNie->length == pRSNie->rsnIEdata[1] + 2) &&
                  (pRSNie->rsnIEdata[0] == SIR_MAC_WPA_EID))
@@ -345,7 +355,16 @@ limSetRSNieWPAiefromSmeStartBSSReqMessage(tpAniSirGlobal pMac,
                    FL("Only WPA IE is present"));
 
             dot11fUnpackIeWPA(pMac,&pRSNie->rsnIEdata[6],(tANI_U8)pRSNie->length-4,
+                              pRSNie->rsnIEdata[1] - 4,
                                 &pSessionEntry->gStartBssWPAIe);
+            if (DOT11F_FAILED(status))
+            {
+                limLog(pMac,
+                       LOGE,FL("unpack failed for WPA IE (0x%08x)"),
+                       status);
+                return false;
+            }
+	    return true;
         }
 
         // Check validity of WPA IE
@@ -372,10 +391,22 @@ limSetRSNieWPAiefromSmeStartBSSReqMessage(tpAniSirGlobal pMac,
                 /* Both RSN and WPA IEs are present */
                 dot11fUnpackIeRSN(pMac,&pRSNie->rsnIEdata[2],
                       (tANI_U8)pRSNie->length,&pSessionEntry->gStartBssRSNIe);
+                if (DOT11F_FAILED(status))
+                {
+                    limLog(pMac,LOGE,FL("unpack failed for RSN IE status:(0x%08x)"),
+                               status);
+                    return false;
+                }
 
                 dot11fUnpackIeWPA(pMac,&pRSNie->rsnIEdata[wpaIndex + 6],
                                  pRSNie->rsnIEdata[wpaIndex + 1]-4,
                                     &pSessionEntry->gStartBssWPAIe);
+                if (DOT11F_FAILED(status))
+                {
+                    limLog(pMac, LOGE,FL("unpack failed for WPA IE status:(0x%08x)"),
+                               status);
+                    return false;
+                }
 
             }
         }
@@ -671,6 +702,10 @@ limIsSmeJoinReqValid(tpAniSirGlobal pMac, tpSirSmeJoinReq pJoinReq)
     tANI_U8 valid = true;
 
 
+    //If force_rsne_override is enabled that mean User has provided the
+    //test RSNIE which need to be send as it is in assoc req and thus RSNIE
+    //validity is not required.
+    if (!pJoinReq->force_rsne_override &&
     if (!limIsRSNieValidInSmeReqMessage(pMac, &pJoinReq->rsnIE))
     {
         limLog(pMac, LOGE,
